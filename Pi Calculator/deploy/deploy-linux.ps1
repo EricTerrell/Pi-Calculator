e035e1cb1e6e4adc5fefa04704c1b1d50692d81f
Eric Terrell <eric@terrell.info>
2025-03-21 08:56:42 -0600

    Updated main window layout

---
+++ Pi Calculator/deploy/deploy-linux.ps1
@@ -0,0 +1,6 @@
+﻿c:
+cd "\Users\erict\Documents\software development\Pi Calculator\Pi Calculator"
+
+Remove-Item "C:\Users\erict\Documents\software development\Pi Calculator\Calculate Pi\bin\Release\net9.0\linux-x64" -Recurse -Force
+
+dotnet publish -c Release --os linux --self-contained -f net9.0
